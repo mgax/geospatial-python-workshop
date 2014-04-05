1245e1de56a99de3fbb05b69e5686c37c411a706
Alex Morega <alex@grep.ro>
2014-04-05 11:14:07 +0300

    update example

--- intro/localitati.py
+++ intro/localitati.py
@@ -4,7 +4,7 @@ from pyproj import Proj
 stereo70 = Proj(init='epsg:31700')
 
 for line in csv.DictReader(open('localitati.csv')):
-    if line['judet'] == 'CJ':
+    if line['judet'] == 'Cluj':
         (x, y) = line['xy'].split(',')
         (lat, lng) = stereo70(int(x), int(y), inverse=True)
         print '%.2f' % lat, '%.2f' % lng, line['nume']
