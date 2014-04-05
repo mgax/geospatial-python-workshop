e5be4b912e291733df2883c00a68d2707e93b12a
Alex Morega <alex@grep.ro>
2014-04-05 11:26:44 +0300

    count sheep and cattle

--- intro/localitati.py
+++ intro/localitati.py
@@ -3,8 +3,16 @@ from pyproj import Proj
 
 stereo70 = Proj(init='epsg:31700')
 
+vaci = 0
+oi = 0
+
 for line in csv.DictReader(open('localitati.csv')):
-    if line['judet'] == 'Cluj':
+    if line['judet'] == 'Bucuresti':
         (x, y) = line['xy'].split(',')
         (lat, lng) = stereo70(int(x), int(y), inverse=True)
         print '%.2f' % lat, '%.2f' % lng, line['nume']
+        vaci += int(line['vaci'])
+        oi += int(line['oi'])
+
+print 'vaci:', vaci
+print 'oi:', oi
